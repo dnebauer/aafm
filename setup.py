c8b6065907fc655dd06e11fac5c7da50844a31d8
David Nebauer <david@nebauer,org>
2016-07-17 16:11:02 +0930

    Make pylint happy

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python2.7
 
+""" setup.py: build and install aafm site package """
+
 import sys
 from distutils.core import setup
 
-prefix=sys.prefix + '/bin'
+PREFIX = sys.prefix + '/bin'
 
 setup(name='aafm',
       version='0.4',
@@ -15,12 +17,14 @@ setup(name='aafm',
       packages=['aafm'],
       package_dir={'aafm': 'src'},
       package_data={'aafm': ['data/*/*']},
-      data_files=[('/usr/share/icons/hicolor/32x32/apps', ['icon/32/aafm.png']),
-                  ('/usr/share/icons/hicolor/48x48/apps', ['icon/48/aafm.png']),
-                  ('/usr/share/icons/hicolor/64x64/apps', ['icon/64/aafm.png']),
-                  ('/usr/share/icons/hicolor/128x128/apps', ['icon/128/aafm.png']),
-                  ('/usr/share/icons/hicolor/256x256/apps', ['icon/256/aafm.png']),
-                  ('/usr/share/icons/hicolor/scalable/apps', ['icon/scalable/aafm.svg']),
-                  ('/usr/share/applications', ['aafm.desktop']),
-                  (prefix , ['aafm'])]
-      )
+      data_files=[
+          ('/usr/share/icons/hicolor/32x32/apps', ['icon/32/aafm.png']),
+          ('/usr/share/icons/hicolor/48x48/apps', ['icon/48/aafm.png']),
+          ('/usr/share/icons/hicolor/64x64/apps', ['icon/64/aafm.png']),
+          ('/usr/share/icons/hicolor/128x128/apps', ['icon/128/aafm.png']),
+          ('/usr/share/icons/hicolor/256x256/apps', ['icon/256/aafm.png']),
+          ('/usr/share/icons/hicolor/scalable/apps',
+           ['icon/scalable/aafm.svg']),
+          ('/usr/share/applications', ['aafm.desktop']),
+          (PREFIX, ['aafm'])
+      ])
